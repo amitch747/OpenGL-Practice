dcc7f02c81552e5a0f25a65bad3c6aa296d999db
Aiden Mitchell <aidenmitchell747@gmail.com>
2025-11-16 12:35:25 -0700

    2 tri

--- main.cpp
+++ main.cpp
@@ -99,23 +99,33 @@ int main() {
     // set up vertex data and buffers
     float verticies[] = {
         0.5f, 0.5f, 0.0f, // top right
-        0.5f, -0.5f, 0.0f, // bottom right
-        -0.5f, -0.5f, 0.0f, // bottom left
-        -0.5f, 0.5f, 0.0f   // top left
+        1.0f, -0.5f, 0.0f, // bot right
+
+        0.0, -0.5f, 0.0f, // bottom middle
+
+        -0.5f, 0.5f, 0.0f,  // top left
+        -1.0f, -0.5f, 0.0f, // bot left
+        
     };
     unsigned int indices[] = {
-        0, 1, 3, // first tri
-        1, 2, 3 // 2nd try
+        0, 1, 2, // first tri
+        2, 3, 4 // 2nd try
     };
 
-    unsigned int VBO, VAO;
+
+    unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
     // bind Vertex Array Object first, then bind and set vertex buffer(s), then configure vertex attributes
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(verticies), verticies, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
@@ -131,13 +141,15 @@ int main() {
     {
         processInput(window);
 
-        glClearColor(0.2f, 0.5f, 1.0f, 1.0f);
+        glClearColor(0.8f, 0.8f, 1.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         // draw triangle
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
